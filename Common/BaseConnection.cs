7b29d00dfe8ceb70f3e32d38dcb9d2613fdc953c
Hernanda <m.hernanda95@gmail.com>
2021-03-24 22:49:14 +0700

    init
    mager

---
+++ Common/BaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIConsumerWithRefit.Common
+{
+    public class BaseConnection
+    {
+        public static string TargetAPI => "https://localhost:44338/";
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using APIConsumerWithRefit.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIConsumerWithRefit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("_fullName")))
+            {
+                string fullName = HttpContext.Session.GetString("_fullName");
+
+                ViewBag.fullName = fullName;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "login");
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "login");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
